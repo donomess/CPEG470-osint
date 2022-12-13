989a0dcf3dc4d6398ddccd645bee64f4facaa481
Donovan Messer <donovanmess01@gmail.com>
2022-12-12 21:40:41 -0500

    done?

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ def main():
     check_ans1()
     print("You made it to my University! Good job. \n I wonder what could be next? Do you happen to know the most famous UD alum?\n")
     check_ans2()
+    print("I know! A president! Where did Biden give a campaign speech on October 6th during his run?")
+    check_ans3()
+    print("Good job! You're a pro at this. What's the coordinates of the building where Gettsyburg handle their finances?")
 
 
 def check_ans1():
@@ -19,10 +22,27 @@ def check_ans1():
 def check_ans2():
     ans2 = input("What is the link to their website? (Input it here)\n")
     if ans2 == second_link:
-        print("Good job! You found it. Advancing to the last step. \n")
+        print("Good job! You found it. Advancing to the third step. \n")
         return
     else:
         print("Wrong! Try again.\n")
         check_ans2()
 
+def check_ans3():
+    ans3 = input("Provide the city: ")
+    if ans3 == third_answer:
+        print("Great job! You found this historical town.\n")
+        return
+    else:
+        print("Wrong! Try again.\n")
+        check_ans3()
+
+def check_ans4():
+    ans4 = input("Provide the coords: (+/-##.##,+/-##.##)")
+    if ans4 == password:
+        print("Congrats! You've worked so hard. Here's your flag: " + flag + "}")
+        return
+    else:
+        print("Wrong! Try again.\n")
+        check_ans4()
 main()
